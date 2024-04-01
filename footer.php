f1cf579ffe2f913ae5cc81b9c771ada276d716e0
kirito908 <153584084+kirito908@users.noreply.github.com>
2024-04-01 11:47:42 +0545

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy; Hamro Salon 2024 @Bipin&Pukar</p>
+    </div>
+        <!--//footer-->
---
+++ sidebar.php
@@ -0,0 +1,73 @@
+  
+<style type="text/css">
+  #leftCol {
+    position: fixed;
+    width: 309px;
+    overflow-y: scroll;
+    top: 0;
+    bottom: 0;
+}
+</style>
+
+  <div class=" sidebar" role="navigation">
+            <div class="navbar-collapse">
+        <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+          <ul class="nav" id="leftCol" style="margin-top:7% ;">
+            <li>
+              <a href="dashboard.php"><i class="fa fa-home nav_icon"></i>Dashboard</a>
+            </li>
+            <li>
+              <a href="add-services.php"><i class="fa fa-cogs nav_icon"></i>Services<span class="fa arrow"></span> </a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="add-services.php">Add Services</a>
+                </li>
+                <li>
+                  <a href="manage-services.php">Manage Services</a>
+                </li>
+              </ul>
+              <!-- /nav-second-level -->
+            </li>
+            <li class="">
+              <a href="about-us.php"><i class="fa fa-book nav_icon"></i>Pages <span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="about-us.php">About Us</a>
+                </li>
+                <li>
+                  <a href="contact-us.php">Contact Us</a>
+                </li>
+              </ul>
+              <!-- /nav-second-level -->
+            </li>
+          
+            <li>
+              <a href="all-appointment.php"><i class="fa fa-check-square-o nav_icon"></i>Appointment<span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="all-appointment.php">All Appointment</a>
+                </li>
+                
+              </ul>
+              <!-- //nav-second-level -->
+            </li>
+           
+        
+            
+             
+             
+        
+            
+   
+            <li>
+              <a href="search-appointment.php" class="chart-nav"><i class="fa fa-search nav_icon"></i>Search Appointment</a>
+            </li>
+           
+          
+
+          </ul>
+          <div class="clearfix"> </div>
+          <!-- //sidebar-collapse -->
+        </nav>
+      </div>
+    </div>
